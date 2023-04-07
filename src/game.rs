9bd794f6cce022ae3f9a8b20f2cb9ac6802d1e31
Govind Srinivasaraghavan <gsrinivasaraghavan@digitalocean.com>
2023-04-07 12:28:28 -0400

    Small cleanups + Use mutable refs

--- src/game.rs
+++ src/game.rs
@@ -2,7 +2,7 @@ use crossterm::style::Color;
 use crossterm::*;
 use crossterm_input::*;
 use crossterm_screen::Screen;
-use std::io::stdout;
+use std::io::{stdout, Stdout};
 use std::{process, thread, time};
 
 use rand::Rng;
@@ -32,14 +32,10 @@ impl Board {
             snake: Snake {
                 head: snake_start,
                 body: vec![],
-                direction: Direction::Up,
+                direction: &Direction::Up,
             },
             food: food_start,
         }
-    }
-
-    pub fn random_position(&self) -> Position {
-        random_position(self.width, self.height)
     }
 
     pub fn is_legal(&self, next_position: Position) -> bool {
@@ -49,12 +45,12 @@ impl Board {
             && next_position.y > 0
     }
 
-    pub fn draw(&self) -> Result<()> {
+    pub fn draw(&self, stdout: &mut Stdout) -> Result<()> {
         for y in 0..self.height {
             for x in 0..self.width {
                 // Check if this is interesting, if so draw it
                 if let Some(direction) = self.snake.at_head(x, y) {
-                    stdout()
+                    stdout
                         .queue(crossterm::cursor::DisableBlinking)
                         .unwrap()
                         .queue(crossterm::cursor::Hide)
@@ -70,7 +66,7 @@ impl Board {
                 }
 
                 if y == 0 || y == (self.height - 1) || x == 0 || x == (self.width - 1) {
-                    stdout()
+                    stdout
                         .queue(crossterm::cursor::DisableBlinking)
                         .unwrap()
                         .queue(crossterm::cursor::Hide)
@@ -79,15 +75,15 @@ impl Board {
                         .unwrap()
                         .queue(crossterm::style::SetColors(crossterm::style::Colors::new(
                             Color::DarkRed,
-                            Color::Black,
+                            Color::Red,
                         )))
                         .unwrap()
-                        .queue(crossterm::style::Print("*"))?;
+                        .queue(crossterm::style::Print(" "))?;
                 }
             }
         }
         // Place the food at it's position by drawing it
-        stdout()
+        stdout
             .queue(crossterm::cursor::DisableBlinking)
             .unwrap()
             .queue(crossterm::cursor::Hide)
@@ -101,7 +97,7 @@ impl Board {
             .unwrap()
             .queue(crossterm::style::Print("@"))?;
 
-        stdout()
+        stdout
             .queue(crossterm::cursor::MoveTo(
                 self.snake.head.x,
                 self.snake.head.y,
@@ -118,12 +114,12 @@ impl Board {
 
 pub fn random_position(width: u16, height: u16) -> Position {
     Position {
-        x: rand::thread_rng().gen_range(0..width - 1),
-        y: rand::thread_rng().gen_range(0..height - 1),
+        x: rand::thread_rng().gen_range(1..width - 1),
+        y: rand::thread_rng().gen_range(1..height - 1),
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Game {
     pub board: Board,
     pub score: u16,
@@ -136,61 +132,87 @@ impl Game {
             score: 0,
         }
     }
-    pub fn tick(&mut self, stdin: &mut AsyncReader) -> Result<()> {
+
+    pub fn tick(&mut self, stdin: &mut AsyncReader, stdout: &mut Stdout) -> Result<()> {
         // Take player input
         // check if it's legal/game is over
         // Draw board
         // Go back to start
+        let mut game_started = false;
+        self.board.draw(stdout)?;
         loop {
+            std::thread::sleep(time::Duration::from_millis(600));
             if let Some(key_event) = stdin.next() {
                 match key_event {
-                    InputEvent::Keyboard(k) => {
-                        self.board.draw()?;
-                        match k {
-                            // KeyEvent::Up => self.board.snake.direction = Direction::Up,
-                            KeyEvent::Up => self.board.snake.grow(Direction::Up)?,
+                    InputEvent::Keyboard(k) => match k {
+                        KeyEvent::Up => {
+                            game_started = true;
+                            self.board.snake.grow(&Direction::Up, stdout)
+                        }
 
-                            KeyEvent::Down => self.board.snake.grow(Direction::Down)?,
-                            KeyEvent::Right => self.board.snake.grow(Direction::Right)?,
-                            KeyEvent::Left => self.board.snake.grow(Direction::Left)?,
-                            KeyEvent::Char('q') => {
-                                stdout().execute(crossterm::terminal::Clear(
-                                    terminal::ClearType::All,
-                                ))?;
-                                let score_message =
-                                    format!("\n Total score is :{:?} \n", self.score);
-                                stdout()
-                                    .queue(crossterm::style::SetColors(
-                                        crossterm::style::Colors::new(
-                                            Color::DarkGreen,
-                                            Color::Black,
-                                        ),
-                                    ))
-                                    .unwrap()
-                                    .queue(crossterm::style::Print(score_message))?;
-
-                                stdin.stop_reading();
-                                std::process::exit(0);
-                            }
-                            _ => {
-                                println!("Use Up/Down/Let/Right arrow keys to play the game. Press 'q' to quit");
+                        KeyEvent::Down => {
+                            game_started = true;
+                            self.board.snake.grow(&Direction::Down, stdout)
+                        }
+                        KeyEvent::Right => {
+                            game_started = true;
+                            self.board.snake.grow(&Direction::Right, stdout)
+                        }
+                        KeyEvent::Left => {
+                            game_started = true;
+                            self.board.snake.grow(&Direction::Left, stdout)
+                        }
+                        KeyEvent::Char('q') => {
+                            stdin.stop_reading();
+                            self.quit(stdout)?;
+                        }
+                        _ => {
+                            if game_started {
+                                self.board.snake.grow(self.board.snake.direction, stdout);
                             }
                         }
-                        if self.board.food == self.board.snake.head {
-                            // add food in a new random position and continue
-                            self.board.food = random_position(self.board.width, self.board.height);
-                            self.score = self.score + 1;
-                        }
-                    }
+                    },
                     _ => {}
                 }
+            } else {
+                if game_started {
+                    std::thread::sleep(time::Duration::from_millis(600));
+                    self.board.snake.grow(self.board.snake.direction, stdout);
+                }
             }
-            self.board.draw()?;
+
+            if self
+                .board
+                .snake
+                .collided_with_wall(self.board.height, self.board.width)
+            {
+                self.quit(stdout)?;
+            }
+            if self.board.food == self.board.snake.head {
+                // add food in a new random position and continue
+                self.board.food = random_position(self.board.width - 1, self.board.height - 1);
+                self.score = self.score + 1;
+            }
+            self.board.draw(stdout)?;
         }
+    }
+
+    pub fn quit(&mut self, stdout: &mut Stdout) -> Result<()> {
+        let score_message = format!("\n\n Game Over!!! Total score is :{:?} \n", self.score);
+        stdout.execute(crossterm::terminal::Clear(terminal::ClearType::All))?;
+        stdout
+            .queue(crossterm::style::SetColors(crossterm::style::Colors::new(
+                Color::DarkGreen,
+                Color::Black,
+            )))
+            .unwrap()
+            .queue(crossterm::style::Print(score_message))?;
+
+        std::process::exit(0);
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum Direction {
     Up,
     Down,
@@ -201,7 +223,7 @@ pub enum Direction {
 #[derive(Debug, Clone)]
 pub struct Snake {
     pub head: Position,
-    pub direction: Direction,
+    pub direction: &'static Direction,
     pub body: Vec<Position>,
 }
 
@@ -218,8 +240,7 @@ impl Snake {
         return None;
     }
 
-    pub fn grow(&mut self, direction: Direction) -> Result<()> {
-        stdout().execute(crossterm::terminal::Clear(terminal::ClearType::All))?;
+    pub fn grow(&mut self, direction: &'static Direction, stdout: &mut Stdout) {
         match direction {
             Direction::Up => {
                 self.head.y = self.head.y - 1;
@@ -235,6 +256,9 @@ impl Snake {
             }
         }
         self.direction = direction;
-        Ok(())
+    }
+
+    pub fn collided_with_wall(&mut self, x: u16, y: u16) -> bool {
+        self.head.x == 0 || self.head.y == 0 || self.head.y == y - 1 || self.head.x == x - 1
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod game;
 
 use crate::game::Game;
+use crossterm::style::Color;
 use crossterm::*;
 use crossterm_input::*;
 use crossterm_screen::Screen;
@@ -10,13 +11,15 @@ use std::io::stdout;
 
 fn main() -> Result<()> {
     let mut game = Game::new(24, 24);
-
-    stdout().execute(crossterm::terminal::Clear(terminal::ClearType::All))?;
+    let mut stdout = stdout();
     let screen = Screen::new(true);
-    let mut input = TerminalInput::from_output(&screen.stdout);
-    input.disable_mouse_mode();
+    let input = TerminalInput::from_output(&screen.stdout);
+    input.disable_mouse_mode()?;
     let mut stdin = input.read_async();
+
+    stdout.execute(crossterm::terminal::Clear(terminal::ClearType::All))?;
+    game.board.draw(&mut stdout)?;
     loop {
-        let _ = game.tick(&mut stdin);
+        let _ = game.tick(&mut stdin, &mut stdout);
     }
 }
